12d8f77419581491c4a22d5db139a56826dbc99d
George D. Rodakino <57292409+gdroda@users.noreply.github.com>
2026-02-19 23:40:53 +0200

    test2

--- ShoppingList.Server/Controllers/AuthController.cs
+++ ShoppingList.Server/Controllers/AuthController.cs
@@ -42,7 +42,7 @@ namespace ShoppingList.Server.Controllers
             {
                 var name = User.Identity.Name;
                 var email = User.FindFirst(c => c.Type == ClaimTypes.Email)?.Value;
-                var user = await _userServices.GetUser(ame);
+                var user = await _userServices.GetUser(name);
                 if (user != null)
                 {
                     return Ok(user);
